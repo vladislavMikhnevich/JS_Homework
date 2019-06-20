6ab5881243289b34aa347260b3fbfb800637f217
Vladislav Mikhnevich <mihnevich_vlad@mail.ru>
2019-06-20 17:14:37 +0300

    Homework 6

--- Lesson-6/main.js
+++ Lesson-6/main.js
@@ -19,36 +19,44 @@ barsik.feed()
 
 // Task 2
 
+﻿
+﻿
 function Cat(name) {
-    this.name = name;
-    var foodAmount = 50;
-
+    function Cat(name) {
+        this.name = name;
+        var foodAmount = 50;
+​
     this.DailyNorm = function(amount) {
-
+    ​
         if (amount > 500) {
             throw new Error('Нельзя насыпать корма, больше чем 500 гр.');
         }
-
+​
         if (amount < 50) {
             throw new Error('Нельзя насыпать корма, меньше чем 50 гр.');
         }
-
+​
+        if (!amount) {
+            throw new Error('Введите корректное значение');
+        }
+​
         foodAmount = amount;
-
+​
         if(!arguments.lenght) return foodAmount;
-
+​
     };
-
+​
     function getformatFoodAmount() {
         return foodAmount + 'гр.';
     };
-
+​
     this.feed = function() {
         console.log('Насыпаем в миску ' + getformatFoodAmount() + ' корма');
     };
-}
-
+    }
+​
 var barsik = new Cat('Barsik');
+​
+barsik.DailyNorm();
+    barsik.feed();
 
-barsik.DailyNorm(505);
-barsik.feed();
